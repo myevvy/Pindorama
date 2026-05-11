5a487e248516798080a223f49fa5bab2cae727f3
Evelyn Aquiles Silva de França <168788742+myevvy@users.noreply.github.com>
2026-05-11 23:17:54 +0000

    js

--- script/scriptFloresta.js
+++ script/scriptFloresta.js
@@ -1,70 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let inputPesquisa = document.querySelector("#pesquisa_texto");
-    console.log(inputPesquisa)
-    let filtros = document.querySelectorAll("#botoes-filtro .filtros");
-    let noticias = document.querySelectorAll("#bloco-textos .blocos");
-    let filtroSelecionado = "todos";
-    let resultadosTexto = document.getElementById("textoResultados");
-    let resultados = 0;
+  let inputPesquisa = document.querySelector("#pesquisa_texto");
+  let filtros = document.querySelectorAll("#botoes-filtro .filtros");
+  let noticias = document.querySelectorAll("#blocos-textos .blocos");
+  let filtroSelecionado = "todos";
+  let resultadosTexto = document.getElementById("textoResultados");
+  let resultados = 0;
 
-        filtros.forEach((filtro) => {
-            filtro.addEventListener("click", () => {
-                filtros.forEach((f) => {
-                    f.classList.remove("selecionado");
-                });
-    
-                if (filtroSelecionado == filtro.dataset.filtro) {
-                    filtroSelecionado = "todos";
-                    const todos = document.querySelector('[data-filtro="todos"]');
-                    todos.classList.add("selecionado");
-                } else {
-                    filtro.classList.add("selecionado");
-                    filtroSelecionado = filtro.dataset.filtro;
-                }
-                buscarNoticia(
-                    inputPesquisa.value.toLowerCase().replace(/\s+/g, ""),
-                    filtroSelecionado,
-                );
-            });
-        });
+  filtros.forEach((filtro) => {
+    filtro.addEventListener("click", () => {
+      filtros.forEach((f) => {
+        f.classList.remove("selecionado");
+      });
 
-    inputPesquisa.addEventListener("input", () => {
-        buscarNoticia(
-            inputPesquisa.value.toLowerCase().replace(/\s+/g, ""),
-            filtroSelecionado,
-        );
+      if (filtroSelecionado == filtro.dataset.filtro) {
+        filtroSelecionado = "todos";
+        const todos = document.querySelector('[data-filtro="todos"]');
+        todos.classList.add("selecionado");
+      } else {
+        filtro.classList.add("selecionado");
+        filtroSelecionado = filtro.dataset.filtro;
+      }
+      buscarNoticia(
+        inputPesquisa.value.toLowerCase().replace(/\s+/g, ""),
+        filtroSelecionado,
+      );
     });
+  });
 
-    function buscarNoticia(pesquisa, filtroSelecionado) {
-        pesquisa = pesquisa.toLowerCase();
-        noticias.forEach((noticia) => {
-            let categoria = noticia.dataset.categoria;
-            let titulo = noticia
-                .querySelector(".titulos-blocos")
-                .textContent.toLowerCase()
-                .replace(/\s+/g, "");
-            let desc = noticia
-                .querySelector(".textinhos")
-                .textContent.toLowerCase()
-                .replace(/\s+/g, "");
+  inputPesquisa.addEventListener("input", () => {
+    buscarNoticia(
+      inputPesquisa.value.toLowerCase().replace(/\s+/g, ""),
+      filtroSelecionado
+    );
+  });
 
-            let filtroTexto = titulo.includes(pesquisa) || desc.includes(pesquisa);
-            let filtroCategoria =
-                categoria === filtroSelecionado || filtroSelecionado === "todos";
+  function buscarNoticia(pesquisa, filtroSelecionado) {
+    pesquisa = pesquisa.toLowerCase();
+    noticias.forEach((noticia) => {
+      let categoria = noticia.dataset.categoria;
+      let titulo = noticia
+        .querySelector(".titulos-blocos")
+        .textContent.toLowerCase()
+        .replace(/\s+/g, "");
+      let desc = noticia
+        .querySelector(".textinhos")
+        .textContent.toLowerCase()
+        .replace(/\s+/g, "");
 
-            if (filtroTexto && filtroCategoria) {
-                noticia.style.display = "flex";
-                resultados++;
-            } else {
-                noticia.style.display = "none";
-            }
-        });
-        resultadosTexto.textContent = resultados;
-        resultados = 0;
-    }
+      let filtroTexto = titulo.includes(pesquisa) || desc.includes(pesquisa);
+      let filtroCategoria =
+        categoria === filtroSelecionado || filtroSelecionado === "todos";
 
-    buscarNoticia(inputPesquisa.value, filtroSelecionado);
+      if (filtroTexto && filtroCategoria) {
+        noticia.style.display = "flex";
+        resultados++;
+      } else {
+        noticia.style.display = "none";
+      }
+    });
+    resultadosTexto.textContent = resultados;
+    resultados = 0;
+  }
 
-  
-    
+  buscarNoticia(inputPesquisa.value, filtroSelecionado);
 });
